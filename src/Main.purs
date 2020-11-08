00a07a6655e79272dcbb64f900f9c26ff1aa1e01
matoruru <matoruru@shgt.jp>
2020-11-08 16:54:23 +0900

    Initial commiti

--- src/Main.purs
+++ src/Main.purs
@@ -1,253 +1,98 @@
 module Main where
 
 import Prelude
-import Prim.TypeError
-import Type.Row
 
-import Data.Maybe (Maybe(..), optional)
+import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
-import Effect (Effect)
-import Effect.Console (log, logShow)
-import Main4 as Main4
 import Prim.Row as Row
 import Prim.RowList as RL
 import Record as Record
 import Record.Builder (Builder)
 import Record.Builder as Builder
-import Type.Equality (class TypeEquals)
-import Type.Prelude (class IsSymbol, Proxy(..), RLProxy(..), RProxy(..))
-import Type.RowList (class ListToRow) as RL
-import Unsafe.Coerce (unsafeCoerce)
+import Type.Prelude (class IsSymbol, Proxy(..), RLProxy(..))
 
 data Required a
 
-class Sort
-  (i :: RL.RowList) (o :: RL.RowList)
-  | i -> o
-
-instance sort ::
-  ( RL.ListToRow i i'
-  , RL.RowToList i' o
-  ) => Sort i o
-
 class ExtractRequiredFromDefinition
-  (spec :: RL.RowList) (initial :: RL.RowList) (result :: RL.RowList)
-  | spec initial -> result
+  (spec :: RL.RowList) (result :: # Type)
+  | spec -> result
 
-instance _ExtractRequiredFromDefinitionNil ::
-  ( Sort initial initial'
-  ) => ExtractRequiredFromDefinition RL.Nil initial initial'
+instance i_ExtractRequiredFromDefinitionNil ::
+  ExtractRequiredFromDefinition RL.Nil ()
 
-instance _ExtractRequiredFromDefinitionCons ::
-  ( ExtractRequiredFromDefinition tail (RL.Cons label a initial) result
-  ) => ExtractRequiredFromDefinition (RL.Cons label (Required a) tail) initial result
+instance i_ExtractRequiredFromDefinitionIgnore ::
+  ( ExtractRequiredFromDefinition tail result'
+  , Row.Cons label a result' result
+  ) => ExtractRequiredFromDefinition (RL.Cons label (Required a) tail) result
 
-else instance _ExtractRequiredFromDefinitionIgnore ::
-  ( ExtractRequiredFromDefinition tail initial result
-  ) => ExtractRequiredFromDefinition (RL.Cons label a tail) initial result
+else instance i_ExtractRequiredFromDefinitionCons ::
+  ( ExtractRequiredFromDefinition tail result
+  ) => ExtractRequiredFromDefinition (RL.Cons label a tail) result
 
 class ExtractOptionalFromDefinition
-  (spec :: RL.RowList) (initial :: RL.RowList) (result :: RL.RowList)
-  | spec initial -> result
+  (spec :: RL.RowList) (result :: # Type)
+  | spec -> result
 
-instance _ExtractOptionalFromDefinitionNil ::
-  ( Sort initial initial'
-  ) => ExtractOptionalFromDefinition RL.Nil initial initial'
+instance i_ExtractOptionalFromDefinitionNil ::
+  ExtractOptionalFromDefinition RL.Nil ()
 
-instance _ExtractOptionalFromDefinitionIgnore ::
-  ( ExtractOptionalFromDefinition tail initial result
-  ) => ExtractOptionalFromDefinition (RL.Cons label (Required a) tail) initial result
+instance i_ExtractOptionalFromDefinitionIgnore ::
+  ( ExtractOptionalFromDefinition tail result
+  ) => ExtractOptionalFromDefinition (RL.Cons label (Required a) tail) result
 
-else instance _ExtractOptionalFromDefinitionCons ::
-  ( ExtractOptionalFromDefinition tail (RL.Cons label a initial) result
-  ) => ExtractOptionalFromDefinition (RL.Cons label a tail) initial result
+else instance i_ExtractOptionalFromDefinitionCons ::
+  ( ExtractOptionalFromDefinition tail result'
+  , Row.Cons label a result' result
+  ) => ExtractOptionalFromDefinition (RL.Cons label a tail) result
 
-class CompareToDefinition
-  (required :: RL.RowList) (optional :: RL.RowList) (input :: RL.RowList)
-
-instance _CompareToDefinitionNil ::
-  CompareToDefinition RL.Nil RL.Nil RL.Nil
-
-instance _CompareToDefinitionRequired ::
-  ( CompareToDefinition tail optional tail'
-  ) => CompareToDefinition (RL.Cons label a tail) optional (RL.Cons label a tail')
-
-else instance _CompareToDefinitionOptional ::
-  ( CompareToDefinition required tail tail'
-  ) => CompareToDefinition required (RL.Cons label a tail) (RL.Cons label a tail')
-
-else instance _CompareToDefinitionIgnoreOptional ::
-  ( CompareToDefinition required tail input
-  ) => CompareToDefinition required (RL.Cons label a tail) input
-
-class Builder
-  (required :: RL.RowList) (optional :: RL.RowList) (input :: RL.RowList) input' from to
-  | required optional input input' from -> to
+mapRecord :: forall mytype mytype' opts opts' reqs optsreqs input xs merged
+   . RL.RowToList opts xs
+  => MapRecord xs () opts'
+  => Row.Nub merged optsreqs
+  => Row.Union input opts' merged
+  => RL.RowToList mytype mytype'
+  => ExtractOptionalFromDefinition mytype' opts
+  => ExtractRequiredFromDefinition mytype' reqs
+  => Row.Union opts' reqs optsreqs
+  => Proxy (Record mytype)
+  -> Record input
+  -> Record optsreqs
+mapRecord _ r = ((Record.merge r $ (Builder.build builder {} :: Record opts')) :: Record optsreqs)
   where
-    builder :: RLProxy required -> RLProxy optional -> RLProxy input -> Record input' -> Builder.Builder { | from } { | to }
+    builder = mapRecordBuilder (RLProxy :: _ xs)
 
-instance _BuilderRequiredNil ::
-  Builder RL.Nil RL.Nil RL.Nil input' from from where
-    builder _ _ _ _ = identity
+class MapRecord (xs :: RL.RowList) (from :: # Type) (to :: # Type)
+  | xs -> from to where
+  mapRecordBuilder :: RLProxy xs -> Builder { | from } { | to }
 
-instance _BuilderRequired ::
-  ( IsSymbol label
-  , Builder tail optional tail' deleted from from'
-  , Row.Cons label a deleted input'
-  , Row.Cons label a from' to
-  , Row.Lacks label from'
-  , Row.Lacks label deleted
-  ) => Builder (RL.Cons label a tail) optional (RL.Cons label a tail') input' from to where
-    builder _ _ _ r = first <<< rest
-      where
-        label   = SProxy :: _ label
-        value   = Record.get label r :: a
-        deleted = Record.delete label r
-        first   = Builder.insert label value
-        rest    = builder (RLProxy :: _ tail) (RLProxy :: _ optional) (RLProxy :: _ tail') deleted
+instance mapRecordCons ::
+  ( IsSymbol name
+  , MapRecord tail from from'
+  , Row.Lacks name from'
+  , Row.Cons name (Maybe a) from' to
+  ) => MapRecord (RL.Cons name a tail) from to where
+  mapRecordBuilder _ =
+    first <<< rest
+    where
+      nameP = SProxy :: _ name
+      rest = mapRecordBuilder (RLProxy :: _ tail)
+      first = Builder.insert nameP (Nothing :: _ a)
 
-else instance _BuilderOptionalJust ::
-  ( IsSymbol label
-  , Builder required tail tail' deleted from from'
-  , Row.Cons label a deleted input'
-  , Row.Cons label (Maybe a) from' to
-  , Row.Lacks label from'
-  , Row.Lacks label deleted
-  ) => Builder required (RL.Cons label a tail) (RL.Cons label a tail') input' from to where
-    builder _ _ _ r = first <<< rest
-      where
-        label   = SProxy :: _ label
-        value   = Just $ Record.get label r :: Maybe a
-        deleted = Record.delete label r
-        first   = Builder.insert label value
-        rest    = builder (RLProxy :: _ required) (RLProxy :: _ tail) (RLProxy :: _ tail') deleted
+instance mapRecordNil :: MapRecord RL.Nil () () where
+  mapRecordBuilder _ = identity
 
-else instance _BuilderOptionalNothing ::
-  ( IsSymbol label
-  , Builder required tail input input' from from'
-  , Row.Cons label (Maybe a) from' to
-  , Row.Lacks label from'
-  ) => Builder required (RL.Cons label a tail) input input' from to where
-    builder _ _ _ r = first <<< rest
-      where
-        label   = SProxy :: _ label
-        value   = Nothing :: Maybe a
-        first   = Builder.insert label value
-        rest    = builder (RLProxy :: _ required) (RLProxy :: _ tail) (RLProxy :: _ input) r
-
-extract
-  :: forall spec spec' required required' optional optional'
-   . TypeEquals (RProxy MyRecord) (RProxy spec)
-  => RL.RowToList spec spec'
-  => ExtractRequiredFromDefinition spec' RL.Nil required'
-  => ExtractOptionalFromDefinition spec' RL.Nil optional'
-  => RL.ListToRow required' required
-  => RL.ListToRow optional' optional
-  => TypeEquals { | required } { c :: Int, d :: Number }
-  => TypeEquals { | optional } { a :: Int, b :: String, e :: String }
-  => {}
-extract = {}
-
-extract' :: {}
-extract' = extract
-
-confirm
-  :: forall spec spec' input input' required' optional'
-   . TypeEquals (RProxy MyRecord) (RProxy spec)
-  => TypeEquals (RProxy ( a :: Int, c :: Int, d :: Number, e :: String )) (RProxy input)
-  => RL.RowToList spec spec'
-  => RL.RowToList input input'
-  => ExtractRequiredFromDefinition spec' RL.Nil required'
-  => ExtractOptionalFromDefinition spec' RL.Nil optional'
-  => CompareToDefinition required' optional' input'
-  => {}
-confirm = {}
-
-confirm' :: {}
-confirm' = confirm
-
-class Build
-  (spec :: # Type) (input :: # Type) (output :: # Type)
-  | spec -> output, output -> spec
-  where
-    build :: Record input -> Record output
-
-instance i_Build ::
-  ( RL.RowToList spec spec'
-  , RL.RowToList input input'
-  , ExtractRequiredFromDefinition spec' RL.Nil required'
-  , ExtractOptionalFromDefinition spec' RL.Nil optional'
-  , CompareToDefinition required' optional' input'
-  , Builder required' optional' input' input () output
-  ) => Build spec input output where
-    build r = Builder.build builder' {}
-      where
-        builder' = builder (RLProxy :: _ required') (RLProxy :: _ optional') (RLProxy :: _ input') r
-
-type MyRecord =
+type MyType =
   ( a :: Int
   , b :: String
-  , c :: Required Int
-  , d :: Required Number
-  , e :: String
+  , c :: Number
+  , d :: String
+  , e :: Required Int
+  , f :: Required String
   )
 
-build1
-  :: forall i o
-   . Build MyRecord i o
-  => Record i
-  -> Record o
-build1 = build
+type Constraints result input output =
+  forall trash. Row.Union input result trash => Row.Nub trash output => Record input -> Record output
 
-class Cuild
-  (spec :: # Type) (input :: # Type) (output :: # Type) (a :: RL.RowList)
-  | spec -> output, output -> spec, spec -> a
+mapRecord' r = result
   where
-    cuild :: Record input -> Record spec -> RLProxy a
-
-instance i_Cuild ::
-  ( RL.RowToList spec spec'
-  , RL.RowToList input input'
-  , ExtractRequiredFromDefinition spec' RL.Nil required'
-  , ExtractOptionalFromDefinition spec' RL.Nil optional'
-  , CompareToDefinition required' optional' input'
-  , Builder required' optional' input' input () output
-  ) => Cuild spec input output required' where
-    cuild _ _ = RLProxy :: _ required'
--- 
--- a
---   :: forall input output a
---    . Cuild MyRecord input output a
---   => Record input
---   -> Record output
---   -> RLProxy a
--- a = cuild
--- 
--- a' :: _
--- a' = a
-
-a :: _
-a r = (r { a: "Hi", b: "aa", c: 3 }).a
-
-a' :: _
-a' = a \x -> { a: 3 }
-
-{-
--}
-
--- -- It still cannot access to field...
--- -- TODO: How can this be inferred?
--- build2
---   :: forall i o
---    . Build MyRecord i o
---   => Record i
---   -> Record o
--- build2 r = result.c
---   where
---     result = build r
-
--- main :: Effect Unit
--- main = do
---   logShow $ build1
-
-main = Main4.main
+    result = mapRecord (Proxy :: _ { | MyType }) r
