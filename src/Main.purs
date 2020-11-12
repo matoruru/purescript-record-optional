58dccc598ec514cce9b9428daa2aa13a22bcd683
matoruru <matoruru@shgt.jp>
2020-11-13 01:14:16 +0900

    Add an example

--- src/Main.purs
+++ src/Main.purs
@@ -3,26 +3,46 @@ module Main where
 import Prelude
 
 import Build (class Build, build)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromMaybe)
 import Effect (Effect)
 import Effect.Console (logShow)
 import Type (type (-*), Required, Spec(..))
-import Type.Prelude (Proxy(..))
 
-type Person =
-  ( name       :: String -* Required
-  , age        :: Int    -* Required
-  , region     :: String -* Required
-  , region2    :: String
-  , middlename :: String
+type PersonSpec =
+  ( name    :: String
+  , age     :: Int
+  , job     :: String
   )
 
-createPerson :: forall input. Build Person input _ => Record input -> _
-createPerson r = result
+type Person =
+  { name    :: Maybe String
+  , age     :: Maybe Int
+  , job     :: Maybe String
+  }
+
+conversation
+  :: forall input
+   . Build PersonSpec input Person
+  => Record input
+  -> String
+conversation input = name <> age <> job
   where
-    result = build (Spec :: _ Person) r
+    person :: Person
+    person = build (Spec :: _ PersonSpec) input
+
+    name :: String
+    name = "Hello, " <> fromMaybe "stranger" person.name <> ". "
+
+    age :: String
+    age = case person.age of
+            Just a  -> "Oh you are " <> show a <> ", cool. "
+            Nothing -> "You don't wanna tell me your age, ok. "
+
+    job :: String
+    job = case person.job of
+            Just j  -> "You are a good " <> j <> "."
+            Nothing -> "Let's work together."
 
 main :: Effect Unit
 main = do
-  logShow $ createPerson { name: "", age: 3, region: "", region2: Just "" }
-  pure unit
+  logShow $ conversation {}
