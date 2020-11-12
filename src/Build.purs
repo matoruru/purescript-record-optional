e33aadc35df1ff4d58bd96dca55502049a7fa6ee
matoruru <matoruru@shgt.jp>
2020-11-13 01:14:03 +0900

    Refactor

--- src/Build.purs
+++ src/Build.purs
@@ -2,14 +2,12 @@ module Build where
 
 import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
-import Prelude (Unit, identity, unit, (<<<))
+import Prelude (identity, (<<<))
 import Prim.Row as Row
 import Prim.RowList as RL
-import Prim.TypeError (class Fail, Beside, Text)
-import Record as Record
 import Record.Builder as Builder
 import Type as T
-import Type.Prelude (class IsSymbol, class TypeEquals, Proxy(..), RLProxy(..), RProxy)
+import Type.Prelude (class IsSymbol, class TypeEquals, RLProxy(..), RProxy)
 
 class Build
   (spec :: # Type) (input :: # Type) (result :: Type)
@@ -37,78 +35,32 @@ class Builder
 instance iBuilderNil :: Builder RL.Nil RL.Nil from from where
     builder _ _ = identity
 
-instance iBuilderRequiredFound ::
-  ( IsSymbol label
-  , Row.Cons label a from' from
-  , Builder tail tail' from to
-  ) => Builder (RL.Cons label (T.Required a) tail) (RL.Cons label a tail') from to where
-    builder _ _ = next <<< Builder.modify label identity
-      where
-        label = SProxy :: _ label
-        next  = builder (RLProxy :: _ tail) (RLProxy :: _ tail')
-
-else instance iBuilderRequiredWrongType ::
+instance iBuilderRequired ::
   ( Row.Cons label a () expected
-  , Row.Cons label a' () actual
-  , TypeEquals { | expected } { | actual }
-  , Builder RL.Nil RL.Nil from to
-  ) => Builder (RL.Cons label (T.Required a) tail) (RL.Cons label a' tail') from to where
-    builder _ _ = builder (RLProxy :: _ RL.Nil) (RLProxy :: _ RL.Nil)
-
-else instance iBuilderRequiredMissing ::
-  ( Fail (Beside (Text "Required field \"") (Beside (Text label) (Beside (Text "\" ") (Text "is missing."))))
-  , Builder RL.Nil RL.Nil from to
+  , Row.Cons label' a' () actual
+  , TypeEquals (RProxy expected) (RProxy actual)
+  , Builder tail tail' from to
   ) => Builder (RL.Cons label (T.Required a) tail) (RL.Cons label' a' tail') from to where
-    builder _ _ = builder (RLProxy :: _ RL.Nil) (RLProxy :: _ RL.Nil)
+    builder _ _ = builder (RLProxy :: _ tail) (RLProxy :: _ tail')
 
 else instance iBuilderOptionalFound ::
-  ( IsSymbol label
-  , Row.Cons label (Maybe a) from' from
+  ( TypeEquals a' (Maybe a'')
+  , TypeEquals a a''
   , Builder tail tail' from to
-  ) => Builder (RL.Cons label a tail) (RL.Cons label (Maybe a) tail') from to where
-    builder _ _ = next <<< Builder.modify label identity
-      where
-        label = SProxy :: _ label
-        next  = builder (RLProxy :: _ tail) (RLProxy :: _ tail')
-
-else instance iBuilderOptionalWrongType ::
-  ( Row.Cons label a () expected
-  , Row.Cons label (Maybe a') () actual
-  , TypeEquals { | expected } { | actual }
-  , Builder RL.Nil RL.Nil from to
-  ) => Builder (RL.Cons label a tail) (RL.Cons label (Maybe a') tail') from to where
-    builder _ _ = builder (RLProxy :: _ RL.Nil) (RLProxy :: _ RL.Nil)
-
-else instance iBuilderOptionalMissingJust ::
-  ( IsSymbol label
-  , TypeEquals a (Maybe a)
-  , Builder RL.Nil RL.Nil from to
-  ) => Builder (RL.Cons label a tail) (RL.Cons label a tail') from to where
-    builder _ _ = builder (RLProxy :: _ RL.Nil) (RLProxy :: _ RL.Nil)
+  ) => Builder (RL.Cons label a tail) (RL.Cons label a' tail') from to where
+    builder _ _ = builder (RLProxy :: _ tail) (RLProxy :: _ tail')
 
 else instance iBuilderOptionalMissing ::
   ( IsSymbol label
   , Row.Lacks label from
   , Row.Cons label (Maybe a) from from'
-  , Builder tail (RL.Cons label' a' tail') from' to
-  ) => Builder (RL.Cons label a tail) (RL.Cons label' a' tail') from to where
+  , Builder tail input from' to
+  ) => Builder (RL.Cons label a tail) input from to where
     builder _ _ = next <<< Builder.insert label value
       where
         label = SProxy :: _ label
         value = Nothing :: Maybe a
-        next  = builder (RLProxy :: _ tail) (RLProxy :: _ (RL.Cons label' a' tail'))
-
-else instance iBuilderOptionalMissingNil ::
-  ( IsSymbol label
-  , Row.Lacks label from
-  , Row.Cons label (Maybe a) from from'
-  , Builder tail RL.Nil from' to
-  ) => Builder (RL.Cons label a tail) RL.Nil from to where
-    builder _ _ = next <<< Builder.insert label value
-      where
-        label = SProxy :: _ label
-        value = Nothing :: Maybe a
-        next  = builder (RLProxy :: _ tail) (RLProxy :: _ RL.Nil)
+        next  = builder (RLProxy :: _ tail) (RLProxy :: _ input)
 
 class FromSpec (spec :: RL.RowList) (result :: # Type) | spec -> result
 
